7fb66d110eeea3b18c622240ae57779a6c793340
xqwtxon <xqwtxon@hotmail.com>
2023-09-24 15:52:36 +0800

    Fix CS

--- src/types/recipe/SmithingTransformRecipe.php
+++ src/types/recipe/SmithingTransformRecipe.php
@@ -17,7 +17,6 @@ namespace pocketmine\network\mcpe\protocol\types\recipe;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
-use function is_null;
 
 final class SmithingTransformRecipe extends RecipeWithTypeId{
 
